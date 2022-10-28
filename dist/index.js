e2ed423144040cd59f9f85e0b6d20ca919b381d8
FrancoT67 <francotoledo1235@gmail.com>
2022-10-27 21:02:02 -0300

    seguir revisando

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-var nameUsr = 'franco';
-var ageUsr = '25';
-console.log("Mi nombre es ".concat(nameUsr, " y mi edad es ").concat(ageUsr));
--- index.ts
+++
@@ -1,5 +0,0 @@
-const nameUsr='franco'
-const ageUsr='25'
-
-console.log(`Mi nombre es ${nameUsr} y mi edad es ${ageUsr}`)
-
